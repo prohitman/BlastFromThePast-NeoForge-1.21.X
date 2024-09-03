151517c1ba43d5b447fb1df19b0f8d6e7670d322
lvgraef <lvgraef@zeelandnet.nl>
2024-09-03 21:11:50 +0200

    Made the stripping work with the modern way without an AT

--- src/main/java/com/clonz/blastfromthepast/init/ModBlocks.java
+++ src/main/java/com/clonz/blastfromthepast/init/ModBlocks.java
@@ -1,10 +1,9 @@
 package com.clonz.blastfromthepast.init;
 
 import com.clonz.blastfromthepast.BlastFromThePast;
-import com.clonz.blastfromthepast.block.CedarLeavesBlock;
-import com.clonz.blastfromthepast.block.CedarLogBlock;
-import com.clonz.blastfromthepast.block.SappyCedarLogBlock;
-import com.clonz.blastfromthepast.block.StrippedCedarLogBlock;
+import com.clonz.blastfromthepast.block.CustomLeavesBlock;
+import com.clonz.blastfromthepast.block.CustomLogBlock;
+import com.clonz.blastfromthepast.block.CustomPlanksBlock;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
@@ -21,27 +20,27 @@ public class ModBlocks {
     public static DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(BlastFromThePast.MODID);
 
-    public static final DeferredBlock<Block> CEDAR_LOG = createRegistry("cedar_log",
-            () -> new CedarLogBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG)), new Item.Properties());
+    public static final DeferredBlock<CustomLogBlock> STRIPPED_CEDAR_LOG = createRegistry("stripped_cedar_log",
+            () -> new CustomLogBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG), null, true, 5, 5), new Item.Properties());
 
-    public static final DeferredBlock<Block> STRIPPED_CEDAR_LOG = createRegistry("stripped_cedar_log",
-            () -> new StrippedCedarLogBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG)), new Item.Properties());
+    public static final DeferredBlock<CustomLogBlock> CEDAR_LOG = createRegistry("cedar_log",
+            () -> new CustomLogBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG), STRIPPED_CEDAR_LOG, true, 5, 5), new Item.Properties());
 
-    public static final DeferredBlock<Block> SAPPY_CEDAR_LOG = createRegistry("sappy_cedar_log",
-            () -> new SappyCedarLogBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG)), new Item.Properties());
+    public static final DeferredBlock<CustomLogBlock> SAPPY_CEDAR_LOG = createRegistry("sappy_cedar_log",
+            () -> new CustomLogBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG), CEDAR_LOG, true, 5, 5), new Item.Properties());
 
     public static final DeferredBlock<Block> CEDAR_PLANKS = createRegistry("cedar_planks",
-            () -> new StrippedCedarLogBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS)), new Item.Properties());
+            () -> new CustomPlanksBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS), true, 5, 5), new Item.Properties());
 
     public static final DeferredBlock<Block> CEDAR_LEAVES = createRegistry("cedar_leaves",
-            () -> new CedarLeavesBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LEAVES)), new Item.Properties());
+            () -> new CustomLeavesBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LEAVES), true, 5, 5), new Item.Properties());
 
     public static final DeferredBlock<Block> CEDAR_DOOR = createRegistry("cedar_door",
             () -> new DoorBlock(BlockSetType.OAK, BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_DOOR)), new Item.Properties());
 
 
-    public static <T extends Block> DeferredBlock<Block> createRegistry(String name, Supplier<T> block, Item.Properties properties) {
-        DeferredBlock<Block> object = BLOCKS.register(name, block);
+    public static <T extends Block> DeferredBlock<T> createRegistry(String name, Supplier<T> block, Item.Properties properties) {
+        DeferredBlock<T> object = BLOCKS.register(name, block);
         ModItems.ITEMS.register(name, () -> new BlockItem(object.get(), properties));
 
         return object;
