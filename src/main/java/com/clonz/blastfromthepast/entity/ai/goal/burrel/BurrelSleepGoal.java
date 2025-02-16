6f0d1c6c4214e8e8c80f4b09d99b30b77f038521
ZigyTheBird <105124180+ZigyTheBird@users.noreply.github.com>
2025-02-16 11:38:30 +0330

    Burrels sleeping should work better now

--- src/main/java/com/clonz/blastfromthepast/entity/ai/goal/burrel/BurrelSleepGoal.java
+++ src/main/java/com/clonz/blastfromthepast/entity/ai/goal/burrel/BurrelSleepGoal.java
@@ -3,32 +3,24 @@ package com.clonz.blastfromthepast.entity.ai.goal.burrel;
 import com.clonz.blastfromthepast.entity.Burrel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
-import net.minecraft.world.entity.ai.goal.WrappedGoal;
+import net.minecraft.world.level.block.state.BlockState;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.EnumSet;
 
 public class BurrelSleepGoal extends Goal {
     private final Burrel burrel;
-    private final List<Goal> removedGoals = new ArrayList<>();
 
     public BurrelSleepGoal(Burrel burrel) {
         this.burrel = burrel;
+        this.setFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK, Goal.Flag.JUMP));
     }
 
     @Override
     public void start() {
-        for (WrappedGoal goal : burrel.goalSelector.getAvailableGoals()) {
-            if (goal.getGoal() instanceof RandomLookAroundGoal || goal.getGoal() instanceof LookAtPlayerGoal) {
-                removedGoals.add(goal.getGoal());
-            }
-        }
-        for (Goal goal : removedGoals) {
-            burrel.goalSelector.removeGoal(goal);
-        }
         burrel.setSleeping(true);
+        burrel.getNavigation().stop();
+        burrel.getMoveControl().setWantedPosition(burrel.getX(), burrel.getY(), burrel.getZ(), 0.0);
+        burrel.setDeltaMovement(0, 0, 0);
     }
 
     @Override
@@ -39,18 +31,17 @@ public class BurrelSleepGoal extends Goal {
 
     @Override
     public boolean canContinueToUse() {
-        return !burrel.isPanicking() && burrel.level().getBlockState(burrel.blockPosition().below()).is(BlockTags.LEAVES) && burrel.level().isNight();
+        return !burrel.isPanicking() && burrelOnTree() && burrel.level().isNight();
     }
-
-    public void tick() {
-        burrel.getNavigation().stop();
+    
+    public boolean burrelOnTree() {
+        BlockState blockState = burrel.level().getBlockState(burrel.blockPosition().below());
+        if (blockState.is(BlockTags.LEAVES)) return true;
+        if (!blockState.isAir()) return false;
+        return burrel.level().getBlockState(burrel.blockPosition().below(2)).is(BlockTags.LEAVES);
     }
 
     public void stop() {
-        for (Goal goal : removedGoals) {
-            burrel.goalSelector.addGoal(5, goal);
-        }
-        removedGoals.clear();
         burrel.setSleeping(false);
     }
 }
